cfce60e837157dd0d3f131bf71ffa1f90a3f68c6
Gurdas Satwani <137665724+gurdasss@users.noreply.github.com>
2026-04-28 14:16:43 +0000

    Enhance interactive input handling with rustyline and add history support

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,45 @@
-use std::io::Write;
+use rustyline::error::ReadlineError;
+use rustyline::{DefaultEditor, Result};
 
-fn main() {
+fn main() -> Result<()> {
+    // The DefaultEditor is a struct provided by the rustyline crate that implements a readline-like interface.
+    // if this returns an Err, immediately return that error from the current function.
+    // If it's Ok, unwrap it and give me the value.
+    let mut rl = DefaultEditor::new()?;
+
+    // Try to load the history from a file, if it fails, print a message but don't panic.
+    #[cfg(feature = "with-file-history")]
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
     // Rust's loop construct is an infinite loop that can be exited with a break statement.
     loop {
-        print!(">> ");
-        // Flush the output to ensure the prompt is displayed before waiting for input.
-        std::io::stdout().flush().unwrap();
-        let mut input = String::new();
-
-        // Read a line of input from the user.
-        // read_line returns Result<usize> where the usize is the number of bytes read.
-        match std::io::stdin().read_line(&mut input) {
-            Ok(0) => {
-                println!("");
-                break;
-            } // EOF reached, exit the loop
-            Ok(_) => {
+        let input = rl.readline(">> ");
+        match input {
+            Ok(line) => {
                 // Process the input
                 // Trim the input to remove any trailing newline characters.
-                match input.trim() {
+                match line.trim() {
                     "exit" => break,
                     "help" => {}
                     _ => {
-                        println!("{}", input.trim());
+                        println!("{}", line.trim());
                     }
                 }
+
+                // try to add history entry, if it fails, print an error message but don't panic.
+                if let Err(err) = rl.add_history_entry(line.as_str()) {
+                    eprintln!("failed to add history entry: {}", err);
+                }
+            }
+            Err(ReadlineError::Eof) => {
+                println!("EOF");
+                break;
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted");
+                break;
             }
             Err(error) => {
                 eprintln!("error: {}", error);
@@ -32,4 +47,13 @@ fn main() {
             }
         }
     }
+
+    // Try to save the history, if it fails, print an error message but don't panic.
+    #[cfg(feature = "with-file-history")]
+    if let Err(err) = rl.save_history("history.txt") {
+        eprintln!("failed to save history: {}", err);
+    }
+
+    // The main function returns a Result type, which is a common way to handle errors in Rust.
+    Ok(())
 }
