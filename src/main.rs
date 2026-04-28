8d50150d3a69f0a957e05200477919de40a847b7
Gurdas Satwani <137665724+gurdasss@users.noreply.github.com>
2026-04-28 13:39:52 +0000

    Implement interactive input loop in main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    // Rust's loop construct is an infinite loop that can be exited with a break statement.
+    loop {
+        print!(">> ");
+        // Flush the output to ensure the prompt is displayed before waiting for input.
+        std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+
+        // Read a line of input from the user.
+        // read_line returns Result<usize> where the usize is the number of bytes read.
+        match std::io::stdin().read_line(&mut input) {
+            Ok(0) => {
+                println!("");
+                break;
+            } // EOF reached, exit the loop
+            Ok(_) => {
+                // Process the input
+                // Trim the input to remove any trailing newline characters.
+                match input.trim() {
+                    "exit" => break,
+                    "help" => {}
+                    _ => {
+                        println!("{}", input.trim());
+                    }
+                }
+            }
+            Err(error) => {
+                eprintln!("error: {}", error);
+                break; // We got an error, let's end the loop
+            }
+        }
+    }
 }
